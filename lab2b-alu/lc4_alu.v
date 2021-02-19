366ca124113b8340dd0b706098366d8a7d82eb50
Spencer J Solit <ssolit@big04.seas.upenn.edu>
2021-02-18 23:08:31 -0500

    passing all tests

--- lab2b-alu/lc4_alu.v
+++ lab2b-alu/lc4_alu.v
@@ -28,35 +28,39 @@ module lc4_decoder(     input wire [15:0] i_insn,
             // jsr subopcode 1, trap 2
       assign alu_ctl[2:0] =
             (((i_insn[15:12] == 4'd1) && (i_insn[5] == 0)) ||
-             ((i_insn[15:12] == 4'd5) && (i_insn[5] == 0))) ? i_insn[5:3] :
-            (((i_insn[15:12] == 4'd1) && (i_insn[5] == 1)) ? 3'b101 :
+                  ((i_insn[15:12] == 4'd5) && (i_insn[5] == 0))) ? i_insn[5:3] :
+            ((i_insn[15:12] == 4'd1) && (i_insn[5] == 1)) ? 3'b101 :
             (((i_insn[15:12] == 4'd10) && (i_insn[5:4] == 2'd3)) ||
-             ((i_insn[15:12] == 4'd5) && (i_insn[5] == 1))) ? 3'b100 :
+                  ((i_insn[15:12] == 4'd5) && (i_insn[5] == 1))) ? 3'b100 :
             (i_insn[15:13] == 3'd3) ? 3'b110 :
             (i_insn[15:12] == 4'd2) ? {1'b0, i_insn[8:7]} :
             ((i_insn[15:12] == 4'd10) && (i_insn[5:4] < 2'd3)) ? {1'b0, i_insn[5:4]} :
             ((i_insn[15:12] == 4'd9) || (i_insn[15:12] == 4'd8) || 
-            (i_insn[15:11] == 5'd8) || (i_insn[15:11] == 5'd24)) ? 3'b000 :
-            ((i_insn[15:12] == 4'd13) ||(i_insn[15:11] == 5'd9)) ? 3'b001 :
-            ((i_insn[15:11] == 5'd25) && (i_insn[15:12] == 4'd0) ? 3'b111 :
+                 (i_insn[15:11] == 5'd8) || (i_insn[15:11] == 5'd24)) ? 3'b000 :
+            ((i_insn[15:12] == 4'd13) || (i_insn[15:11] == 5'd9)) ? 3'b001 :
+            ((i_insn[15:11] == 5'd25) || (i_insn[15:12] == 4'd0)) ? 3'b111 :
+            (i_insn[15:12] == 4'd15) ? 3'b010:
             3'b000;
 endmodule
 
 module lc4_alu_arith(   input wire [15:0] A,
                         input wire [15:0] B,
                         input wire [2:0] alu_ctl,
+                        input wire [15:0] insn,
                         input wire [15:0] pc,
                         output wire [15:0] out);
       wire cla_cin;
       wire [15:0] cla_A_input, cla_B_input;
-      assign cla_cin = (alu_ctl[2:0] == 3'd2) ? 1'b1 : 1'b0;
+      assign cla_cin = (alu_ctl[2:0] == 3'd2 || alu_ctl[2:0] == 3'd7) ? 1'b1 : 1'b0;
       assign cla_A_input = (alu_ctl[2:0] == 3'd7) ? pc : A;
       assign cla_B_input = (alu_ctl[2:0] == 3'd2) ? ~B:
-                           (alu_ctl[2:0] == 3'd5) ? { {11{B[4]}}, B[4:0] }:
-                           (alu_ctl[2:0] == 3'd6) ? { {10{B[5]}}, B[5:0] }:
-                           (alu_ctl[2:0] == 3'd7) ? { {5{B[10]}}, B[10:0] }:
+                           (alu_ctl[2:0] == 3'd5) ? { {11{insn[4]}}, insn[4:0] }:
+                           (alu_ctl[2:0] == 3'd6) ? { {10{insn[5]}}, insn[5:0] }:
+                           (alu_ctl[2:0] == 3'd7) ? { {5{insn[10]}}, insn[10:0] }:
                            B;
-      
+      wire [15:0] branch_or_b;
+      assign branch_or_b = (insn[15:12] == 4'd0) ? {{7{insn[8]}}, insn[8:0]} : cla_B_input;
+
       wire [15:0] quotient, remainder, cla_output;
       lc4_divider div (
             .i_dividend(A),
@@ -66,7 +70,7 @@ module lc4_alu_arith(   input wire [15:0] A,
       );
       cla16 cla (
             .a(cla_A_input),
-            .b(cla_B_input),
+            .b(branch_or_b),
             .cin(cla_cin),
             .sum(cla_output)
       );
@@ -79,21 +83,25 @@ endmodule
 
 module lc4_alu_logic(   input wire [15:0] A,
                         input wire [15:0] B,
+                        input wire [15:0] insn,
                         input wire [2:0] alu_ctl,
                         output wire [15:0] out);
       assign out = (alu_ctl[2:0] == 3'd0) ? A & B:
                    (alu_ctl[2:0] == 3'd1) ? ~ A:
                    (alu_ctl[2:0] == 3'd2) ? A | B:
                    (alu_ctl[2:0] == 3'd3) ? A ^ B:
-                   A & { {11{B[4]}}, B[4:0] };
+                   A & { {11{insn[4]}}, insn[4:0] };
 endmodule
 
 module lc4_alu_compare( input wire [15:0] A,
                         input wire [15:0] B,
                         input wire [2:0] alu_ctl,
+                        input wire [15:0] insn,
                         output wire [15:0] out);
-      wire [15:0] sext_B6, res0, res1, res2, res3;
-      assign sext_B6 = { {9{B[6]}}, B[6:0]};
+      wire [15:0] sext_B6, uext_B6, res0, res1, res2, res3;
+      assign sext_B6 = { {9{insn[6]}}, insn[6:0]};
+      assign uext_B6 = { {9{1'b0}}, insn[6:0]};
+      
 
       assign res0 = ($signed(A) < $signed(B)) ? {16{1'b1}}:
                     ($signed(A) == $signed(B)) ? 16'd0 : 16'd1;
@@ -104,8 +112,8 @@ module lc4_alu_compare( input wire [15:0] A,
       assign res2 = ($signed(A) < $signed(sext_B6)) ? {16{1'b1}}:
                     ($signed(A) == $signed(sext_B6)) ? 16'd0 : 16'd1;
 
-      assign res3 = (A < sext_B6) ? {16{1'b1}}:
-                    (A == sext_B6) ? 16'd0 : 16'd1;
+      assign res3 = (A < uext_B6) ? {16{1'b1}}:
+                    (A == uext_B6) ? 16'd0 : 16'd1;
       
       assign out = (alu_ctl[2:0] == 3'd0) ? res0:              //signed all
                    (alu_ctl[2:0] == 3'd1) ? res1:              //unsigned all
@@ -114,13 +122,13 @@ module lc4_alu_compare( input wire [15:0] A,
 endmodule
 
 
-module lc4_alu_shifter( input wire [15:0] A,
+module lc4_alu_shifter( input wire signed [15:0] A,
                         input wire [15:0] B,
                         input wire [2:0] alu_ctl,
-                        output wire [15:0] out);
-      assign out = (alu_ctl[2:0] == 3'd0) ? A << B:
-                   (alu_ctl[2:0] == 3'd1) ? A >>> B:
-                   A >> B;
+                        output wire signed [15:0] out);
+      assign out = (alu_ctl[2:0] == 3'd0) ? A << B[3:0]:
+                   (alu_ctl[2:0] == 3'd1) ? A >>> B[3:0]:
+                   A >> B[3:0];
 endmodule
 
 
@@ -141,7 +149,7 @@ module lc4_alu_jump(    input wire [15:0] i_r1data,
       wire [15:0] jsrr, jsr, trap;                    
       assign jsrr = i_r1data;                                           // also for jmpr, rti
       assign jsr = (i_pc & 16'h8000) | {1'b0, i_insn[10:0], {4{1'b0}}};  
-      assign trap = 16'h8000 | i_insn[7:0];
+      assign trap = 16'h8000 | {{8{1'b0}}, i_insn[7:0]};
       assign out = (alu_ctl == 3'd0) ? jsrr:
                    (alu_ctl == 3'd1) ? jsr:
                    trap;
@@ -157,40 +165,45 @@ module lc4_alu(input  wire [15:0] i_insn,
       
       wire [15:0] arith_out, logic_out, compare_out, shifter_out, const_out, jump_out;
       wire [5:0] alu_ctl; // need to make a decoder to get this
+      
 
       lc4_decoder decoder(
             .i_insn(i_insn),
             .alu_ctl(alu_ctl[5:0])
       );
 
+      
       lc4_alu_arith arith(
             .A(i_r1data),
             .B(i_r2data),
             .pc(i_pc),
+            .insn(i_insn),
             .alu_ctl(alu_ctl[2:0]),
             .out(arith_out)
       );
       lc4_alu_logic logic(
             .A(i_r1data),
             .B(i_r2data),
+            .insn(i_insn),
             .alu_ctl(alu_ctl[2:0]),
             .out(logic_out)
       );
       lc4_alu_compare compare(
             .A(i_r1data),
             .B(i_r2data),
+            .insn(i_insn),
             .alu_ctl(alu_ctl[2:0]),
             .out(compare_out)
       );
       lc4_alu_shifter shift(
             .A(i_r1data),
-            .B(i_r2data),
+            .B(i_insn),
             .alu_ctl(alu_ctl[2:0]),
             .out(shifter_out)
       );
       lc4_alu_const const(
             .A(i_r1data),
-            .B(i_r2data),
+            .B(i_insn),
             .alu_ctl(alu_ctl[2:0]),
             .out(const_out)
       );
