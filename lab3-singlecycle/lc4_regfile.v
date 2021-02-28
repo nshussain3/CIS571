2330383617d48f003e7ee4847c745cfd0714cc0f
Spencer J Solit <ssolit@big04.seas.upenn.edu>
2021-02-28 01:01:09 -0500

    copied files from old assigments

--- lab3-singlecycle/lc4_regfile.v
+++ lab3-singlecycle/lc4_regfile.v
@@ -1,4 +1,5 @@
-/* TODO: name and PennKeys of all group members here
+/* TODO: Names of all group members
+ * TODO: PennKeys of all group members
  *
  * lc4_regfile.v
  * Implements an 8-register register file parameterized on word size.
@@ -26,5 +27,38 @@ module lc4_regfile #(parameter n = 16)
    /***********************
     * TODO YOUR CODE HERE *
     ***********************/
+    wire [7:0] reg_one_hot, reg_we_bus;
+    wire [n-1:0] reg_out_bus [7:0];
+    genvar i;
+    for (i = 0; i < 8; i = i + 1) begin
+        assign reg_one_hot[i] = (i_rd == i) ? 1'b1 : 1'b0;
+        assign reg_we_bus[i] = reg_one_hot[i] & i_rd_we;
+        Nbit_reg reg_i (
+            .in(i_wdata), 
+            .out(reg_out_bus[i]),
+            .clk(clk),
+            .we(reg_we_bus[i]),
+            .gwe(gwe),
+            .rst(rst)
+        );
+        defparam reg_i.n = 16;
+    end
 
+    assign o_rs_data = (i_rs == 3'd0) ? reg_out_bus[0] :
+                (i_rs == 3'd1) ? reg_out_bus[1] :
+                (i_rs == 3'd2) ? reg_out_bus[2] :
+                (i_rs == 3'd3) ? reg_out_bus[3] :
+                (i_rs == 3'd4) ? reg_out_bus[4] :
+                (i_rs == 3'd5) ? reg_out_bus[5] :
+                (i_rs == 3'd6) ? reg_out_bus[6] :
+                reg_out_bus[7];
+    
+    assign o_rt_data = (i_rt == 3'd0) ? reg_out_bus[0] : 
+                (i_rt == 3'd1) ? reg_out_bus[1] : 
+                (i_rt == 3'd2) ? reg_out_bus[2] : 
+                (i_rt == 3'd3) ? reg_out_bus[3] : 
+                (i_rt == 3'd4) ? reg_out_bus[4] : 
+                (i_rt == 3'd5) ? reg_out_bus[5] : 
+                (i_rt == 3'd6) ? reg_out_bus[6] : 
+                reg_out_bus[7];
 endmodule
